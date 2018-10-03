6d02bdb7ac1b463b12352d8b0c646003c86b3353
Chad Kreutzer <kanthalion@gmail.com>
2018-10-03 10:29:17 +0000

    Create a Variable: var done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Create-a-Variable:var.js
@@ -0,0 +1,9 @@
+var favoriteFood = 'pizza';
+var numOfSlices = 8;
+
+console.log(favoriteFood);
+console.log(numOfSlices);
+
+/* Fair enough. They are right. There is sitll a LOT of code out there that
+uses var. and If we aren't at least teaching basic familiarity with it, newbies
+could be confused or put off when they come across it. */
