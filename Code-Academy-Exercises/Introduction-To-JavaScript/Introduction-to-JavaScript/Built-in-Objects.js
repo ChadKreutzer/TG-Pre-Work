4fb3c87a0efa2ea4791ac510f2500d7b6ee63e26
Chad Kreutzer <kanthalion@gmail.com>
2018-10-03 10:20:28 +0000

    Built in Objects done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Built-in-Objects.js
@@ -0,0 +1,9 @@
+console.log(Math.floor(Math.random() * 100));
+
+console.log(Math.ceil(43.8));
+
+console.log(Number.isInteger(2017));
+
+/* Well, I can see how this gives people some tools to play with variables
+before introducing them. Also I like how it refers learners to my favorite
+JavaScript resource: MDN. */
