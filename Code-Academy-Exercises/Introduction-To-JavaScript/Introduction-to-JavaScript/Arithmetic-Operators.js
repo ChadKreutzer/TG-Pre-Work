c024e3c8e90deda49507f6f27c6bd44f14902cf0
Chad Kreutzer <kanthalion@gmail.com>
2018-10-03 10:06:20 +0000

    Arithmatic Operators done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Arithmetic-Operators.js
@@ -0,0 +1,7 @@
+console.log(43 + 3.5);
+console.log(2018 - 1969);
+console.log(65 / 240);
+console.log(0.2708 * 100);
+
+/* I'm kinda disappointed they didn't have a cutsy description for the division
+log. */
