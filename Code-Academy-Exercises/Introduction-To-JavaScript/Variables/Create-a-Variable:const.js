1ef36ace95eefd8b47e2d893d31a42e3ae48071f
Chad Kreutzer <kanthalion@gmail.com>
2018-10-03 10:39:18 +0000

    Create a Variable: const done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Create-a-Variable:const.js
@@ -0,0 +1,11 @@
+const entree = 'Enchiladas';
+
+console.log(entree);
+
+const testing;
+
+/* I love const. I think it is probably from my dabbling in functional
+programming. I just think it is safer and more consistent than using let
+and it tends to enforce better program desgin if you are not changing your
+variable willy nilly. That being said, I'll do whatever is called for by the
+style guide of where I work once I get a job in the field */
