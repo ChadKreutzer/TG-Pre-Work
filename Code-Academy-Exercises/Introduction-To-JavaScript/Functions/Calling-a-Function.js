b0b04d2f182007cfe52ecae024c85fe8ca7772ae
Chad Kreutzer <kanthalion@gmail.com>
2018-10-03 12:58:17 +0000

    Calling a Function done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Calling-a-Function.js
@@ -0,0 +1,10 @@
+function sayThanks() {
+  console.log('Thank you for your purchase! We appreciate your business.');
+}
+
+for (let i = 0; i < 3; i++){
+ sayThanks(); 
+}
+
+/* I decided to use a loop instead of just calling it three times even though I
+haven't "Learned" them yet. */
