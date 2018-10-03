35b6fbbf3d7e128e1bb21b334c93839978db33bd
Chad Kreutzer <kanthalion@gmail.com>
2018-10-03 10:25:12 +0000

    Variables

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Variables.js
@@ -0,0 +1,5 @@
+/*
+Okay, so they have a whole set of lessons just on variables. I get it now.
+I am interested to see what they have to say about var. My philosophy of late
+has been to default to const, using let only when I have to, and never use var
+*/
