4303fa9402c52b1538b5607a9a0ece75a48db95a
Chad Kreutzer <kanthalion@gmail.com>
2018-10-03 11:17:57 +0000

    Truthy and Falsy Assignment done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Truthy-and-Falsy-Assignment.js
@@ -0,0 +1,9 @@
+let tool = '';
+tool = 'marker';
+// Use short circuit evaluation to assign  writingUtensil variable below:
+let writingUtensil = tool || 'pen';
+
+console.log(`The ${writingUtensil} is mightier than the sword.`);
+
+/* Providing useful tools early on. This is one that I had to figure out on
+my own from seeing other people use it in their code. */
