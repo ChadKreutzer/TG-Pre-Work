7b11beccd673409b667a94a21199bf6cf04df50c
Chad Kreutzer <kanthalion@gmail.com>
2018-10-03 11:09:24 +0000

    Comparison Operators done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Comparison-Operators.js
@@ -0,0 +1,10 @@
+let hungerLevel = 7;
+
+if (hungerLevel > 7) {
+  console.log('Time to eat!');
+} else {
+  console.log('We can eat later!');
+}
+
+/* I was wrong, comparisons was next. probably to give something more
+interesting to play with for the else if */
