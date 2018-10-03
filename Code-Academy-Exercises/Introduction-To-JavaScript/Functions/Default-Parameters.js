6bce610af3ad94f473798ed215a87bd6a73b674d
Chad Kreutzer <kanthalion@gmail.com>
2018-10-03 14:24:13 +0000

    Default Parameters done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Default-Parameters.js
@@ -0,0 +1,8 @@
+function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs') {
+  console.log(`Remember to buy ${item1}`);
+  console.log(`Remember to buy ${item2}`);
+  console.log(`Remember to buy ${item3}`);
+}
+
+/* I kinda expected more to this lesson, like showing overriding of the defaluts,
+but I guess not. */
