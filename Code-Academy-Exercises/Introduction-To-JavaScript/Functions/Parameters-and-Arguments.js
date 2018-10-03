1658abb6dfda163034c3e4e73d0545d58349caa8
Chad Kreutzer <kanthalion@gmail.com>
2018-10-03 14:19:20 +0000

    Parameters and Arguments done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Parameters-and-Arguments.js
@@ -0,0 +1,8 @@
+function sayThanks(name) {
+  console.log(`Thank you for your purchase ${name}! We appreciate your business.`);
+}
+
+sayThanks('Cole');
+
+/* Now we are finally passing something in to the function, although we have
+still not gotten anything out of it. */
