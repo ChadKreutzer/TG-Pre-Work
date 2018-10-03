5b287c0d3f7a10717f11e04e959daffe806c056c
Chad Kreutzer <kanthalion@gmail.com>
2018-10-03 10:52:31 +0000

    String Interpolation done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/String-Interpolation.js
@@ -0,0 +1,7 @@
+const myName = 'Chad Kreutzer';
+const myCity = "London";
+
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
+
+/* Ahh... Much better. Granted, concatination still has its place, but most of
+the time, I prefer this. It just looks so much better and is easier to read*/
