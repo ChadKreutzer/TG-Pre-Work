158efb035f57ee89681f1b124b02b6ea817aebde
Chad Kreutzer <kanthalion@gmail.com>
2018-10-03 11:02:52 +0000

    if else Statments done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/if-else-Statements.js
@@ -0,0 +1,11 @@
+let sale = true;
+
+sale = false;
+
+if(sale) {
+  console.log('Time to buy!');
+} else {
+  console.log('Time to wait for a sale.');
+}
+
+/* Gradually building up. I'm betting else if is next */
