5b032d2046d0ba4f07e2bcda4b9ead7323676ab6
Chad Kreutzer <kanthalion@gmail.com>
2018-10-03 10:49:20 +0000

    String Concatination with Variables done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/String-Concatenation-with-Variables.js
@@ -0,0 +1,6 @@
+const favoriteAnimal = "dog";
+
+console.log('My favorite animal: ' + favoriteAnimal);
+
+/* I think I see a lesson using template literals coming around the bend in the
+file names */
