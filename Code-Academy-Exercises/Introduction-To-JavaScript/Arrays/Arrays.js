623fefe024e8b47a4d512f7fd5fc255d0aa169d1
Chad Kreutzer <kanthalion@gmail.com>
2018-10-04 07:02:44 +0000

    Arrays done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/Arrays.js
@@ -0,0 +1,13 @@
+const newYearsResolutions = [
+  'Keep a journal',
+  'Take a falconry class',
+  'Learn to juggle'
+];
+
+console.log("New Year's Resolutions:");
+newYearsResolutions.forEach(
+  (resolution, index) => console.log(`${index + 1}: ${resolution}`)
+);
+
+/* I don't see the difference. I think the log looks just like the real life
+list. :D */
