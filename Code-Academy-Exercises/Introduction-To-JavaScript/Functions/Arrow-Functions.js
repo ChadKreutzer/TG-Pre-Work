cfb698d99b2e356c21fe4ebdc480ccdd993a6142
Chad Kreutzer <kanthalion@gmail.com>
2018-10-04 06:17:02 +0000

    Arrow Functions done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Arrow-Functions.js
@@ -0,0 +1,13 @@
+const plantNeedsWater = day => {
+  if (day === 'Wednesday') {
+    return true;
+  }
+  else {
+    return false;
+  }
+};
+
+/* This is what they had me do, but I usally feel that if you are going to need
+brackets, it is clearer to just use a traditional function keyword. That being
+said, as I did in the last lesson, you can make a nice, neat, punchy arrow
+function to do this */
