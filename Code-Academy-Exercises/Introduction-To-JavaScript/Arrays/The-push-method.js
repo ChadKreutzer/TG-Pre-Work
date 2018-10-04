374a5be5aed3347ca417da23cf986eafb4b2d983
Chad Kreutzer <kanthalion@gmail.com>
2018-10-04 08:32:44 +0000

    The Push Method done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/The-push-method.js
@@ -0,0 +1,7 @@
+const chores = ['wash dishes', 'do laundry', 'take out trash'];
+
+chores.push('mow the lawn');
+chores.push('cook dinner');
+console.log(chores);
+
+/* Time to play with the stack */
