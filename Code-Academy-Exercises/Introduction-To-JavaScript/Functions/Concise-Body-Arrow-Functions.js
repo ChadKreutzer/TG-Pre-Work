04d70e4e3fd0d0a523987fa87dccf4341c40970c
Chad Kreutzer <kanthalion@gmail.com>
2018-10-04 06:19:44 +0000

    Concise Body Arrow Functions

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Concise-Body-Arrow-Functions.js
@@ -0,0 +1,4 @@
+const plantNeedsWater = day => day === 'Wednesday' ? true : false;
+
+/* And that is why they insisted on the if else in the last lesson. They were
+building up to the single line arrow function. */
