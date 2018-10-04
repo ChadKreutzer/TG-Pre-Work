3fddc264fc73e9c71cbe30ecd7864a17760d1662
Chad Kreutzer <kanthalion@gmail.com>
2018-10-04 06:43:10 +0000

    Block Scope done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Scope/Block-Scope.js
@@ -0,0 +1,12 @@
+function logVisibleLightWaves() {
+  const lightWaves = 'Moonlight';
+  
+  console.log(lightWaves);
+}
+
+logVisibleLightWaves();
+console.log(lightWaves);
+
+/* When explaining block scope, I usually say for people to imagine brackets
+around the whole page for global scope, as that is just the biggest block.
+(until you start talking about exporting modules ;-) ) */
