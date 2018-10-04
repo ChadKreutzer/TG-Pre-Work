77cc0335b7110d3c1fd7ae110e092c8a822d9bb7
Chad Kreutzer <kanthalion@gmail.com>
2018-10-04 06:38:13 +0000

    Global Scope done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Scope/Global-Scope.js
@@ -0,0 +1,9 @@
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+const stars = 'North Star';
+
+const callMyNightSky = () => `Night Sky: ${satellite}, ${stars}, and ${galaxy}`;
+
+console.log(callMyNightSky());
+
+/* Globals. Generally to be avoided, but occasionally necessary */
