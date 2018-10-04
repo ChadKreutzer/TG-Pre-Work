e9bacdced71d1b38b68707a11c87f204656f3c42
Chad Kreutzer <kanthalion@gmail.com>
2018-10-04 08:58:20 +0000

    Repeating Tasks Manually done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Repeating-Tasks-Manually.js
@@ -0,0 +1,9 @@
+// Write your code below
+const vacationSpots = ['New York City', 'London', 'Tokyo'];
+
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
+
+/* They are not wrong. It would be a pain, in fact nearly impossible, to
+program if you didn't have access to looping. */
