f17a739305a78d2c260b515e5efa231ac63ddce4
Chad Kreutzer <kanthalion@gmail.com>
2018-10-04 11:08:51 +0000

    Do While Statement done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Do-While-Statements.js
@@ -0,0 +1,10 @@
+// Write your code below
+const cupsOfSugarNeeded = 6;
+let cupsAdded = 0;
+
+do {
+  cupsAdded++;
+}
+while(cupsAdded < cupsOfSugarNeeded);
+
+/* not much to say. This can be used to validate info gotten from a user */
