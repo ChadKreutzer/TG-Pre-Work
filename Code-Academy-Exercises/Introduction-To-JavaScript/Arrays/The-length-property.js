709996a14837ee937b001d6636e96cb56dd2d531
Chad Kreutzer <kanthalion@gmail.com>
2018-10-04 08:27:07 +0000

    The Length Property

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/The-length-property.js
@@ -0,0 +1,7 @@
+const objectives = ['Learn a new languages', 'Read 52 books', 'Run a marathon'];
+
+console.log(objectives.length);
+
+/* Kinda a review from using the length method earlier. Granted then it was
+used on strings and I don't remember them explicitly saying you could use it
+on Arrays too. */
