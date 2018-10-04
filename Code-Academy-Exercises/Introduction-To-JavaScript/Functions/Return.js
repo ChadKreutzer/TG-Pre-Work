78bc18cd917136a7f91bcb7f4d5148202dff3775
Chad Kreutzer <kanthalion@gmail.com>
2018-10-04 05:59:53 +0000

    Return done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Return.js
@@ -0,0 +1,7 @@
+const monitorCount = (rows, columns) => rows * columns;
+
+const numOfMonitors = monitorCount(5, 4);
+
+console.log(numOfMonitors);
+
+/* For the fun of it, I made the function an expression and an arrow function. */
