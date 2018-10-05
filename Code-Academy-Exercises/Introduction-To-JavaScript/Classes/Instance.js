50e0f7b8636ce8e22d9874c29198d94c6daa1824
Chad Kreutzer <kanthalion@gmail.com>
2018-10-05 09:42:07 +0000

    Instance done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Instance.js
@@ -0,0 +1,12 @@
+class Surgeon {
+  constructor(name, department) {
+    this.name = name;
+    this.department = department;
+  }
+}
+
+const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
+const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
+
+/* creating instances of Surgeon. I do see why this followed the lessons on
+factory functions, this should be kinda repetition with modification. */
