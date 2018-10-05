c9bb1eab254e1b160cbb0051e3378b4ed432ebd5
Chad Kreutzer <kanthalion@gmail.com>
2018-10-05 08:54:54 +0000

    Setters done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Advanced-Objects/Setters.js
@@ -0,0 +1,26 @@
+const robot = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  _numOfSensors: 15,
+  get numOfSensors(){
+    if(typeof this._numOfSensors === 'number'){
+      return this._numOfSensors;
+    } else {
+      return 'Sensors are currently down.'
+    }
+  },
+  set numOfSensors(num) {
+    if (typeof num === 'number' && num >= 0) {
+      this._numOfSensors = num;
+    } else {
+      console.log('Pass in a number that is greater than or equal to 0');
+    }
+  }
+};
+
+robot.numOfSensors = 100;
+console.log(robot.numOfSensors)
+
+/* So get and set make it prettier, but JS is still only pretending to be a C#,
+or Java, or Ruby cause the properties are still not truely private and depend
+on the honor system for others to not directly modify them. */
