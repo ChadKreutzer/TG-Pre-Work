bf803c64787cd7b735df61edfbbcbfd52f4b18c7
Chad Kreutzer <kanthalion@gmail.com>
2018-10-05 10:39:28 +0000

    Introduction done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/Introduction.js
@@ -0,0 +1,13 @@
+var pasta = "Spaghetti"; // ES5 syntax
+
+const meat = "Pancetta"; // ES6 syntax
+
+let sauce = "Eggs and cheese"; // ES6 syntax
+
+// Template literals, like the one below, were introduced in ES6
+const carbonara = `You can make carbonara with ${pasta}, ${meat}, and a sauce made with ${sauce}.`;
+
+/* This'll be fun. I've used babel and webpack, but it has been a minute since
+I've been playing with C# more recently. Also I never really had an official
+"here is how you use it." I just kinda figured it out as I went with a little
+bit of tutorial here and there. */
