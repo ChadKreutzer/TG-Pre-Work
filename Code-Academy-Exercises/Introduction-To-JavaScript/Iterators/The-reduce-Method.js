6b8e5477034fe38469459fb12307c2a89bafce69
Chad Kreutzer <kanthalion@gmail.com>
2018-10-05 05:45:31 +0000

    The reduce Method done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Iterators/The-reduce-Method.js
@@ -0,0 +1,12 @@
+const newNumbers = [1, 3, 5, 7];
+
+const newSum = newNumbers.reduce(function(accumulator, currentValue) {
+  console.log('The value of accumulator: ', accumulator);
+  console.log('The value of currentValue', currentValue);
+  return accumulator + currentValue;
+}, 10);
+
+console.log(newSum);
+
+/* A little tedius with the console.logging but understandable. Array.reduce()
+is probably the hardest of these to grok. But also super powerful */
