2a2e14c9ae77cc0dda790253916a8a17cbb208ce
Chad Kreutzer <kanthalion@gmail.com>
2018-10-05 09:32:42 +0000

    Introduction to Classes done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Introduction-to-Classes.js
@@ -0,0 +1,4 @@
+/* I love classes! I know there are people who don't like them and insist on
+using factory functions, but I don't find the arguments to be that convincing.
+Yes, I know they are just syntactic sugar, but they make your code so clean and
+easy to read. */
