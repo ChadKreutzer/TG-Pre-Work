becdf70eadd306fcf7828a38c48edf5c529bb49e
Chad Kreutzer <kanthalion@gmail.com>
2018-10-05 11:24:32 +0000

    Transpilation with Babel done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/Transpilation-With-Babel.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var pasta = "Spaghetti"; // ES5 syntax
+
+var meat = "Pancetta"; // ES6 syntax
+
+var sauce = "Eggs and cheese"; // ES6 syntax
+
+// Template literals, like the one below, were introduced in ES6
+var carbonara = "You can make carbonara with " + pasta + ", " + meat + ", and a sauce made with " + sauce + ".";
+
+/* This does make it easy to make your code compatible with older browsers while
+still using the new hotness of es6 */
