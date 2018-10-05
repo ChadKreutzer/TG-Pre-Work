19f284f125485e6c4133e758af9599630577b064
Chad Kreutzer <kanthalion@gmail.com>
2018-10-05 06:25:41 +0000

    Bracket Notation done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Bracket-Notation.js
@@ -0,0 +1,15 @@
+const spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  'Active Mission' : true,
+  homePlanet : 'Earth', 
+  numCrew: 5
+ };
+
+const propName =  'Active Mission';
+
+// Write your code below
+const isActive = spaceship['Active Mission'];
+console.log(spaceship[propName]);
+
+/* I like how they are showing via the demo objects how all sorts of data types
+and more can be object properties. */
