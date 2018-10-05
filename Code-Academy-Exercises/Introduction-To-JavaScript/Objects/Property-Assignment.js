44ae52ac65282eb24781dea57d1fc12cce81ca17
Chad Kreutzer <kanthalion@gmail.com>
2018-10-05 06:29:30 +0000

    Property Assignment done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Property-Assignment.js
@@ -0,0 +1,13 @@
+const spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth',
+  color: 'silver',
+  'Secret Mission' : 'Discover life outside of Earth.'
+};
+
+// Write your code below
+spaceship.color = 'glorious gold';
+spaceship.numEngines = 6;
+delete spaceship['Secret Mission'];
+
+/* changing up the properties and values */
