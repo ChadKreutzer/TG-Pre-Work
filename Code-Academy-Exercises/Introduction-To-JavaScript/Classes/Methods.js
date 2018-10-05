612e325f8904e8d747e280cdf981448640967eee
Chad Kreutzer <kanthalion@gmail.com>
2018-10-05 09:48:17 +0000

    Methods done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Methods.js
@@ -0,0 +1,25 @@
+class Surgeon {
+  constructor(name, department) {
+    this._name = name;
+    this._department = department;
+    this._remainingVacationDays = 20;
+  }
+  get name() {
+    return this._name;
+  }
+  get department() {
+    return this._department;
+  }
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+}
+
+const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
+const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
+
+/* These get and set methods remind me of the same things in C#, only not quite
+as polished. (and of course the "private" variables aren't) */
