61cd897403f59e17b6e1b307e5de2927d2966a20
Chad Kreutzer <kanthalion@gmail.com>
2018-10-05 12:40:15 +0000

    Export Default done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/export-default.js
@@ -0,0 +1,18 @@
+const Airplane = {
+  availableAirplanes: [
+    {
+      name: 'AeroJet',
+      fuelCapacity: 800
+    },
+    {
+      name: 'SkyJet',
+      fuelCapacity: 500
+    }
+  ]
+};
+
+export default Airplane;
+
+/* although I've been exposed to them, I haven't really used the es6 export/
+import since I've been mostly using versions of node that don't support it, so
+this is fun getting some practice with it. */
