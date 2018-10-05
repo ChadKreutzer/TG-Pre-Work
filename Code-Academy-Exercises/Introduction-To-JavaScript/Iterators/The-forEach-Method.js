51a7253f8351b26aef344f5b82bc0a69dc79ad52
Chad Kreutzer <kanthalion@gmail.com>
2018-10-05 05:21:28 +0000

    The forEach Method done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Iterators/The-forEach-Method.js
@@ -0,0 +1,7 @@
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits below
+fruits.forEach(fruit => console.log(`I want to eat a ${fruit}`));
+
+/* Similar to what I did playing around earlier to display an array formatted
+as if it was the sample. */
