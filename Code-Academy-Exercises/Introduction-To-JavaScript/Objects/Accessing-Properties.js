65fc5c17ab0b870267beb1fc5883cf64d5aeab67
Chad Kreutzer <kanthalion@gmail.com>
2018-10-05 06:21:17 +0000

    Accessing Properties done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Accessing-Properties.js
@@ -0,0 +1,15 @@
+let spaceship = {
+  homePlanet: 'Earth',
+  color: 'silver',
+  'Fuel Type': 'Turbo Fuel',
+  numCrew: 5,
+  flightPath: ['Venus', 'Mars', 'Saturn']
+};
+
+// Write your code below
+const crewCount = spaceship.numCrew;
+const planetArray = spaceship.flightPath;
+
+/* dot notation on this one. I'm surprised they weren't called "Accessing
+Properties I" and "Accessig Properties II" for this and the Bracket Notation
+lesson */
