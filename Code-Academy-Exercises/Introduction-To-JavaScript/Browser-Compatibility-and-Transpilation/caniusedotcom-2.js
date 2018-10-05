387346dffc622921712cc93b2192421852c2043b
Chad Kreutzer <kanthalion@gmail.com>
2018-10-05 10:47:11 +0000

    caniuse.com 2 done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/caniusedotcom-2.js
@@ -0,0 +1,9 @@
+// Set the variable below to a number
+let esFivePercentageSupport = 95.93;
+
+// Set the variable below to a number
+let esSixTemplateLiterals = 88.51;
+
+/* Honestly, that isn't that bad of a percentage. Granted I'm sure it's been
+some time since this lesson was first published and browsers are constantly
+updated. But it is still good to be as compatible as possible. */
