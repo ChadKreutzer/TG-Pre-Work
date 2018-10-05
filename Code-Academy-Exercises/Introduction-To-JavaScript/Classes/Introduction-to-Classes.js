915600521c22b59ebc95fd3edc362da5b7e6e40f
Chad Kreutzer <kanthalion@gmail.com>
2018-10-05 09:35:25 +0000

    Introduction to Classes done for real

--- Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Introduction-to-Classes.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Introduction-to-Classes.js
@@ -1,3 +1,28 @@
+class Dog {
+  constructor(name) {
+    this._name = name;
+    this._behavior = 0;
+  }
+
+  get name() {
+    return this._name;
+  }
+  get behavior() {
+    return this._behavior;
+  }
+
+  incrementBehavior() {
+    this._behavior++;
+  }
+}
+
+const halley = new Dog('Halley');
+console.log(halley.name); // Print name value to console
+console.log(halley.behavior); // Print behavior value to console
+halley.incrementBehavior(); // Add one to behavior
+console.log(halley.name); // Print name value to console
+console.log(halley.behavior); // Print behavior value to console
+
 /* I love classes! I know there are people who don't like them and insist on
 using factory functions, but I don't find the arguments to be that convincing.
 Yes, I know they are just syntactic sugar, but they make your code so clean and
