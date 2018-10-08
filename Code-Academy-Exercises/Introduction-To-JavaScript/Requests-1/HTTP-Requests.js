6ce12a54f0fe14f7f6e70694863c092efb871b8e
Chad Kreutzer <kanthalion@gmail.com>
2018-10-08 10:42:01 +0000

    HTTP Requests done

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Requests-1/HTTP-Requests.js
@@ -0,0 +1,10 @@
+console.log('First message!');
+setTimeout(() => {
+   console.log('This message will always run last...');
+}, 0);
+console.log('Second message!');
+
+/* A ha! so they are using this to also teach about async. Nice. Good thing too
+because while I do know about async and I have used it, it has been a while. so
+this will be good review for me. And maybe some new stuff as things do change...
+*/
